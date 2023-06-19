d6a5039a32395b148bc2383ccd67105acd9837cb
iSuperstar <40224197+iSuperstar@users.noreply.github.com>
2023-06-18 23:35:20 -0500

    Doing

--- Implementation/Code/Trying Something/automate.sh
+++
@@ -1,3 +0,0 @@
-make runCode
-clear
-for i in $(objdump -d PIC_httpreverse_shell.exe | grep "^ " | cut -f2); do echo -n '\x'$i; done; echo
--- Implementation/Code/Trying Something/output.asm
+++
@@ -1,6 +0,0 @@
-pop r10
-nop 
-add byte ptr [rbx], al
-add byte ptr [rax], al
-add byte ptr [rax + rax], al
-add byte ptr [rax], al
