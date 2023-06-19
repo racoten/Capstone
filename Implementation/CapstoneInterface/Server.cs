81b7223cf62b65083034aed42286e3ac3924b693
iSuperstar <40224197+iSuperstar@users.noreply.github.com>
2023-06-18 23:41:08 -0500

    Update Server.cs

--- Implementation/CapstoneInterface/Server.cs
+++ Implementation/CapstoneInterface/Server.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,18 +10,22 @@ using System.Windows.Forms;
 
 namespace CapstoneInterface
 {
+    // Server class is responsible for the server form of the application.
     public partial class Server : Form
     {
+        // Default constructor for the Server.
         public Server()
         {
             InitializeComponent();
         }
 
+        // Event handler for the button click.
         private void button1_Click(object sender, EventArgs e)
         {
+            // Setting the host and port properties of the formLogin class.
             formLogin.host = txtHost.Text;
             formLogin.port = txtPort.Text;
-            this.Close();
+            this.Close(); // Closing the current form.
         }
     }
 }
