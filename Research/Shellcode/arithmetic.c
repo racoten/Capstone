7b9f5bc4dfca36e74d51f40b07ac9d0412629746
Racoten <vquery.solrac@gmail.com>
2024-01-25 22:26:46 -0400

    d

--- Research/Shellcode/arithmetic.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int a, b = 1;
-	int c = a + b;
-	c += 1;
-	return 0;
-}
--- Research/Shellcode/helloworld_for_assembly.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-char s[] = "Hello World";
-
-int main()
-{
-	int x = 2000, z = 21;
-	printf("%s %d /n", s, x+z);
-}
